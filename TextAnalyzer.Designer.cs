d22024499f5efb4be59921eb3b57743d0229c3f5
Dusty <dustyneubauer@gmail.com>
2023-03-21 12:35:07 -0500

    Commit final text analyzer

---
+++ TextAnalyzer.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace Week6_B
+{
+    partial class TextAnalyzer
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.analyzeButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.numberCheckBox = new System.Windows.Forms.CheckBox();
+            this.specialCharacterCheckBox = new System.Windows.Forms.CheckBox();
+            this.lowerCaseCheckBox = new System.Windows.Forms.CheckBox();
+            this.upperCaseCheckBox = new System.Windows.Forms.CheckBox();
+            this.wordPhraseTextBox = new System.Windows.Forms.TextBox();
+            this.enterTextTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.indexListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.passwordDescription = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.phraseLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // analyzeButton
+            // 
+            this.analyzeButton.Location = new System.Drawing.Point(120, 299);
+            this.analyzeButton.Name = "analyzeButton";
+            this.analyzeButton.Size = new System.Drawing.Size(75, 23);
+            this.analyzeButton.TabIndex = 0;
+            this.analyzeButton.Text = "Analyze";
+            this.analyzeButton.UseVisualStyleBackColor = true;
+            this.analyzeButton.Click += new System.EventHandler(this.analyzeButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(217, 299);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 1;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(314, 299);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // numberCheckBox
+            // 
+            this.numberCheckBox.AutoSize = true;
+            this.numberCheckBox.Enabled = false;
+            this.numberCheckBox.Location = new System.Drawing.Point(428, 231);
+            this.numberCheckBox.Name = "numberCheckBox";
+            this.numberCheckBox.Size = new System.Drawing.Size(70, 19);
+            this.numberCheckBox.TabIndex = 3;
+            this.numberCheckBox.Text = "Number";
+            this.numberCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // specialCharacterCheckBox
+            // 
+            this.specialCharacterCheckBox.AutoSize = true;
+            this.specialCharacterCheckBox.Enabled = false;
+            this.specialCharacterCheckBox.Location = new System.Drawing.Point(428, 197);
+            this.specialCharacterCheckBox.Name = "specialCharacterCheckBox";
+            this.specialCharacterCheckBox.Size = new System.Drawing.Size(117, 19);
+            this.specialCharacterCheckBox.TabIndex = 4;
+            this.specialCharacterCheckBox.Text = "Special Character";
+            this.specialCharacterCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // lowerCaseCheckBox
+            // 
+            this.lowerCaseCheckBox.AutoSize = true;
+            this.lowerCaseCheckBox.Enabled = false;
+            this.lowerCaseCheckBox.Location = new System.Drawing.Point(428, 163);
+            this.lowerCaseCheckBox.Name = "lowerCaseCheckBox";
+            this.lowerCaseCheckBox.Size = new System.Drawing.Size(86, 19);
+            this.lowerCaseCheckBox.TabIndex = 5;
+            this.lowerCaseCheckBox.Text = "Lower Case";
+            this.lowerCaseCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // upperCaseCheckBox
+            // 
+            this.upperCaseCheckBox.AutoSize = true;
+            this.upperCaseCheckBox.Enabled = false;
+            this.upperCaseCheckBox.Location = new System.Drawing.Point(428, 126);
+            this.upperCaseCheckBox.Name = "upperCaseCheckBox";
+            this.upperCaseCheckBox.Size = new System.Drawing.Size(86, 19);
+            this.upperCaseCheckBox.TabIndex = 6;
+            this.upperCaseCheckBox.Text = "Upper Case";
+            this.upperCaseCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // wordPhraseTextBox
+            // 
+            this.wordPhraseTextBox.Location = new System.Drawing.Point(24, 36);
+            this.wordPhraseTextBox.Name = "wordPhraseTextBox";
+            this.wordPhraseTextBox.Size = new System.Drawing.Size(131, 23);
+            this.wordPhraseTextBox.TabIndex = 7;
+            // 
+            // enterTextTextBox
+            // 
+            this.enterTextTextBox.Location = new System.Drawing.Point(24, 85);
+            this.enterTextTextBox.Multiline = true;
+            this.enterTextTextBox.Name = "enterTextTextBox";
+            this.enterTextTextBox.Size = new System.Drawing.Size(263, 185);
+            this.enterTextTextBox.TabIndex = 8;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.BackColor = System.Drawing.Color.White;
+            this.passwordTextBox.Location = new System.Drawing.Point(428, 86);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(100, 23);
+            this.passwordTextBox.TabIndex = 9;
+            this.passwordTextBox.TextChanged += new System.EventHandler(this.passwordTextBox_TextChanged);
+            // 
+            // indexListBox
+            // 
+            this.indexListBox.FormattingEnabled = true;
+            this.indexListBox.ItemHeight = 15;
+            this.indexListBox.Location = new System.Drawing.Point(314, 86);
+            this.indexListBox.Name = "indexListBox";
+            this.indexListBox.Size = new System.Drawing.Size(87, 184);
+            this.indexListBox.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(184, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 15);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Instances of Phrase";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 15);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Enter Phrase or Word";
+            // 
+            // passwordDescription
+            // 
+            this.passwordDescription.AutoSize = true;
+            this.passwordDescription.Location = new System.Drawing.Point(428, 49);
+            this.passwordDescription.Name = "passwordDescription";
+            this.passwordDescription.Size = new System.Drawing.Size(94, 15);
+            this.passwordDescription.TabIndex = 13;
+            this.passwordDescription.Text = "Create Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(314, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 15);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Index of Phrase";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 15);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Enter Text to Search";
+            // 
+            // phraseLabel
+            // 
+            this.phraseLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.phraseLabel.Location = new System.Drawing.Point(187, 36);
+            this.phraseLabel.Name = "phraseLabel";
+            this.phraseLabel.Size = new System.Drawing.Size(100, 23);
+            this.phraseLabel.TabIndex = 16;
+            // 
+            // TextAnalyzer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 377);
+            this.Controls.Add(this.phraseLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.passwordDescription);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.indexListBox);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.enterTextTextBox);
+            this.Controls.Add(this.wordPhraseTextBox);
+            this.Controls.Add(this.upperCaseCheckBox);
+            this.Controls.Add(this.lowerCaseCheckBox);
+            this.Controls.Add(this.specialCharacterCheckBox);
+            this.Controls.Add(this.numberCheckBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.analyzeButton);
+            this.Name = "TextAnalyzer";
+            this.Text = "Text Analysis";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button analyzeButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.CheckBox numberCheckBox;
+        private System.Windows.Forms.CheckBox specialCharacterCheckBox;
+        private System.Windows.Forms.CheckBox lowerCaseCheckBox;
+        private System.Windows.Forms.CheckBox upperCaseCheckBox;
+        private System.Windows.Forms.TextBox wordPhraseTextBox;
+        private System.Windows.Forms.TextBox enterTextTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.ListBox indexListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label passwordDescription;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label phraseLabel;
+    }
+}
+
---
+++ Week6_B/TextAnalyzer.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week6_B
+{
+    public partial class TextAnalyzer : Form
+    {
+
+        //the count instances method counts the number of times the user inputted phrase appears in the 
+        //text and displays the result in a label
+
+        private void CountInstances()
+        {
+            string text = enterTextTextBox.Text;
+            string phrase = wordPhraseTextBox.Text;
+            int count = 0; //holds the count for the number of times the phrase is found
+
+            int position = text.IndexOf(phrase);
+
+            while (position < text.Length)
+            {
+                if (position >= -1)
+                {
+                    indexListBox.Items.Add(position);
+                    count++;
+                }
+                else
+                {
+                    MessageBox.Show("phrase not found");
+                }
+            }
+
+            phraseLabel.Text = count.ToString();
+        }
+
+        private string GetPassword()
+        {
+            string password; //holds the user entered password
+
+            password = passwordTextBox.ToString();
+
+            return password;
+        }
+
+        //the number uppercase reads the number of uppercase characters in the password 
+        private int NumUpperCase()
+        {
+            int upperCase = 0; //holds the number of uppercase characters
+            string password;  //holds the password
+
+            password = GetPassword();
+
+            //count the uppercase characters in password
+            foreach (char ch in password)
+            {
+                if (char.IsUpper(ch))
+                {
+                    upperCase++;
+                }
+            }
+
+            return upperCase;
+        }
+
+        //the number lowercase reads the number of lowercase characters in the password 
+        private int NumLowerCase()
+        {
+            int lowerCase = 0; //holds the number of uppercase characters
+            string password;  //holds the password
+
+            password = GetPassword();
+
+            //count the uppercase characters in password
+            foreach (char ch in password)
+            {
+                if (char.IsLower(ch))
+                {
+                    lowerCase++;
+                }
+            }
+
+            return lowerCase;
+        }
+
+        //the number numdigits reads the number of digits in the password 
+        private int NumDigits()
+        {
+            int digits = 0; //holds the number of uppercase characters
+            string password;  //holds the password
+
+            password = GetPassword();
+
+            //count the uppercase characters in password
+            foreach (char ch in password)
+            {
+                if (char.IsDigit(ch))
+                {
+                    digits++;
+                }
+            }
+
+            return digits;
+        }
+
+        //the number special characters reads the number of special characters in the password 
+        private int NumSpecialCharacter()
+        {
+            int special = 0; //holds the number of special characters
+            string password;  //holds the password
+
+            password = GetPassword();
+
+            //count the special characters in password
+            foreach (char ch in password)
+            {
+                if (char.IsPunctuation(ch))
+                {
+                    special++;
+                }
+            }
+
+            return special;
+        }
+        private void analyzeButton_Click(object sender, EventArgs e)
+        {
+            CountInstances();
+        }
+
+        private void passwordTextBox_TextChanged(object sender, EventArgs e)
+        {
+            //validate the password
+            if (NumLowerCase() >= 1 &&
+                NumUpperCase() >= 1 &&
+                NumDigits() >= 1 &&
+                NumSpecialCharacter() >= 1)
+            {
+                passwordTextBox.BackColor = Color.Green;
+            }
+            else
+            {
+                passwordTextBox.BackColor = Color.Red;
+            }
+
+            //checks the uppercase checkbox
+            if (NumUpperCase() >= 1)
+            {
+                upperCaseCheckBox.Checked = true;
+            }
+            else
+            {
+                lowerCaseCheckBox.Checked = false;
+            }
+
+            //checks the lowercase checkbox
+            if (NumLowerCase() >= 1)
+            {
+                lowerCaseCheckBox.Checked = true;
+            }
+            else
+            {
+                lowerCaseCheckBox.Checked = false;
+            }
+
+            //Checks the digits checkbox
+            if (NumDigits() >= 1)
+            {
+                numberCheckBox.Checked = true;
+            }
+            else
+            {
+                numberCheckBox.Checked = false;
+            }
+
+            //checks the special characters check box
+            if (NumSpecialCharacter() >= 1)
+            {
+                specialCharacterCheckBox.Checked = true;
+            }
+            else
+            {
+                specialCharacterCheckBox.Checked = false;
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            enterTextTextBox.Text = "";
+            wordPhraseTextBox.Text = "";
+            phraseLabel.Text = "";
+            indexListBox.Items.Clear();
+        }
+    }
+
+
+
+}
